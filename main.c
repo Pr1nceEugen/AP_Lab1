474182cd919a4cc9804bcc319cbce2816e200939
Pr1nceEugen <jekavasilenko1995@gmail.com>
2023-10-01 19:57:06 +0200

    AP_Lab1_Result

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <math.h>
+#include <Windows.h>
 
-int main() {
-    printf("Hello, World!\n");
+double FirstFunctionResult(double x, double y)
+{
+    return ((x+2,45)+sqrt(fabs(x+pow(y, 2))));
+}
+
+double SecondFunctionResult(double x, double y, double z)
+{
+    return ((1+y)*(x+y/(pow(x, 2)+4)/(exp(-x-2)+(1/(pow(x,2)+4)))));
+}
+
+double ThirdFunctionResult(double x, double y, double z)
+{
+    return ((1+cos(y-2))/(pow(x,4)/2+pow(sin(z), 2)));
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    double x, y, z;
+    double TemporaryFunctionsResult;
+
+    printf("введіть значення x, y і z через пропуск\n");
+    scanf("%lf%lf%lf",&x, &y, &z);
+
+    TemporaryFunctionsResult = FirstFunctionResult(x, y);
+    printf("шукане значення функції R = %f\n",TemporaryFunctionsResult);
+
+    TemporaryFunctionsResult = SecondFunctionResult(x, y, z);
+    printf("шукане значення функції a = %f\n",TemporaryFunctionsResult);
+
+    TemporaryFunctionsResult = ThirdFunctionResult(x, y, z);
+    printf("шукане значення функції b = %f\n",TemporaryFunctionsResult);
+
     return 0;
 }
